acd6322ecf000bf530e8d4c94302b4f537149161
Elmar Pielmeier <elmar.pielmeier@de.sii.group>
2022-04-19 16:55:43 +0200

    Implemented simple unit test.

--- src/test/java/de/sii/connector/ConnectorApplicationTests.java
+++ src/test/java/de/sii/connector/ConnectorApplicationTests.java
@@ -1,13 +1,24 @@
 package de.sii.connector;
 
+import de.sii.connector.user.controllers.UserCommonErrorController;
+import de.sii.connector.user.controllers.UserController;
+import org.hamcrest.core.IsNull;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest
 class ConnectorApplicationTests {
 
+	@Autowired
+	private UserController userController;
+
 	@Test
 	void contextLoads() {
+		assertThat(userController, is(IsNull.notNullValue()));
 	}
 
 }
