d4f5752647a28891e474c2b8f77f51f89889053e
Elmar Pielmeier <elmar.pielmeier@de.sii.group>
2022-04-19 15:51:35 +0200

    Implemented error page.

--- src/main/java/de/sii/connector/user/controllers/UserCommonErrorController.java
+++ src/main/java/de/sii/connector/user/controllers/UserCommonErrorController.java
@@ -1,21 +1,20 @@
 package de.sii.connector.user.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserCommonErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    @ExceptionHandler(Exception.class)
-    public String handleError(Exception e, Model model) {
+    public String handleError(Error e, Model model) {
         model.addAttribute("message", e.getMessage());
         model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
-        model.addAttribute("error", e);
+        model.addAttribute("error", e.getCause());
 
         return "error";
     }
